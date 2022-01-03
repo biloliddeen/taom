7e5b3b31aff02eb65ca104700091bb16447f3bb5
Biloliddin <biloliddeen@gmail.com>
2022-01-04 00:05:53 +0500

    Fix main layout footer

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -121,11 +121,10 @@ AppAsset::register($this);
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.1.0
-    </div>
+    <?php 
+    $date = date("Y");
+    ?>
+    <strong>Copyright &copy; <?=$date?> <span>Creater</span> <a href="https://github.com/biloliddeen">Bsquare.</a></strong>
   </footer>
 
   <!-- Control Sidebar -->
